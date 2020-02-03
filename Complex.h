9ce5e341cfcf42009dbf221746a97cb0e640d492
Garth Sorenson <garth.sorenson@snow.edu>
2020-02-03 15:38:15 -0700

    corrected file name

--- Complex.h
+++ Complex.h
@@ -1,20 +1,5 @@
 #pragma once
 class Complex
 {
-private:
-	double real_;
-	double imaginary_;
-
-public:
-	Complex(double = 0.0, double = 0.0);
-
-	void setReal(double);
-	void setImaginary(double);
-
-	void print()const;
-
-	Complex add(const Complex&) const;
-	Complex subtract(const Complex&) const;
-
 };
 
